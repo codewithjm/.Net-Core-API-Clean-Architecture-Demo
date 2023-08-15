8a253700b4978bd67228e2f5dad6b6a28bab79fc
Jollymar Obias <jollymar.obias@teleperformance.com>
2023-08-15 23:07:09 +0800

    pipeline behaviors

---
+++ Business/Behaviors/PerformanceLoggingBehavior.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Behaviors;
+
+public class PerformanceLoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+   where TRequest : IRequest<TResponse>
+{
+    private readonly ILogger _logger;
+    private readonly Stopwatch _stopwatch;
+
+    public PerformanceLoggingBehavior(ILogger logger)
+    {
+        _stopwatch = new();
+        _logger = logger;
+    }
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        var name = typeof(TRequest).Name;
+        _logger.Information($"Handling {name}.");
+
+        _stopwatch.Start();
+        var response = await next();
+        _stopwatch.Stop();
+
+        if (_stopwatch.ElapsedMilliseconds <= 3000) return response;
+
+        _logger.Warning("Long Running Request: {name} ({ElapsedMilliseconds} milliseconds).",
+            name, _stopwatch.ElapsedMilliseconds, request);
+
+        return response;
+    }
+}
